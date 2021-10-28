fa8b05476f02c2bf16fac04b893a01bb618af920
thteixeirap <78819692+thteixeirap@users.noreply.github.com>
2021-10-28 12:33:31 -0300

    Add files via upload

---
+++ decks/imagem.js
@@ -0,0 +1,26 @@
+
+// PAGE TESTE
+
+let btn = document.querySelectorAll('.ver');
+let ibage = document.querySelector('.carta')
+let modal = document.querySelectorAll('.modal');
+let modalImg = document.querySelector('#modal_img')
+let btClose = document.querySelectorAll('#bt_close');
+let srcVal = "";
+
+
+  
+alert(ibage.length);
+
+/*
+for(let i = 0; i<imagesPool.length;i++){
+    imagesPool[i].addEventListener('click',function(){
+        srcVal = images[i].getAttribute('src');
+        modalImg.setAttribute('scr',srcVal);
+        alert(i);
+        //modal.classList.toggle('modal_active');
+    });
+}
+*/
+
+
